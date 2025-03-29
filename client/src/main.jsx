e2f3240112fd77a12e929b27009b202c5d69bd8b
sabbirahamedsiam101 <sabbirahamedsiam101@gmail.com>
2025-03-29 20:21:25 +0600

    add axios

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { RouterProvider } from 'react-router'
-import { router } from './routes/Routes.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router";
+import { router } from "./routes/Routes.jsx";
+import AuthProvider from "../providers/AuthProvider.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-   <RouterProvider router={router} />
-  </StrictMode>,
-)
+    <HelmetProvider>
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+    </HelmetProvider>
+  </StrictMode>
+);
